a2548fce96efd79d32fa303f0aaf646daac5867d
insolor <2442833+insolor@users.noreply.github.com>
2025-04-23 19:18:13 +0300

    Use try instead of catch unreachable

--- src/parse_mo.zig
+++ src/parse_mo.zig
@@ -144,7 +144,7 @@ pub fn print_mo(mo_path: []const u8) !void {
 
     var debug_allocator = std.heap.DebugAllocator(.{}){};
     const allocator = debug_allocator.allocator();
-    var iterator = parser.iterateEntries(allocator) catch unreachable;
+    var iterator = try parser.iterateEntries(allocator);
     while (try iterator.next()) |entry| {
         defer entry.deinit(allocator);
 
