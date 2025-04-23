f1567e4fd4981676e1e760f18e73b52941980e34
insolor <2442833+insolor@users.noreply.github.com>
2025-04-23 21:42:51 +0300

    Add a simple cli for print_mo function

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const zul = @import("zul");
+
 const parse_raws = @import("parse_raws.zig");
 const parse_mo = @import("parse_mo.zig");
 
@@ -7,5 +10,30 @@ test {
 }
 
 pub fn main() !void {
-    try parse_mo.print_mo("test_data/test.mo");
+    var da = std.heap.DebugAllocator(.{}){};
+    defer _ = da.deinit();
+    const allocator = da.allocator();
+
+    var args = try zul.CommandLineArgs.parse(allocator);
+    defer args.deinit();
+
+    if (args.count() == 0) {
+        std.debug.print("No arguments passed\n", .{});
+        return;
+    }
+
+    if (args.contains("print_mo")) {
+        const print_mo_arg = args.get("print_mo") orelse {
+            std.debug.print("print_mo arg not found\n", .{});
+            return;
+        };
+
+        if (print_mo_arg.len == 0) {
+            std.debug.print("print_mo arg is empty\n", .{});
+            return;
+        }
+
+        std.debug.print("print_mo arg: {s}\n", .{print_mo_arg});
+        try parse_mo.print_mo(print_mo_arg);
+    }
 }
