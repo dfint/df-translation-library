1ecaea19b3934a5c18be9e9a600e6b5c5f4db3a3
insolor <2442833+insolor@users.noreply.github.com>
2025-05-28 19:38:57 +0300

    Change library linkage mode to dynamic

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
     exe_mod.addImport("df_translation_library_lib", lib_mod);
 
     const lib = b.addLibrary(.{
-        .linkage = .static,
+        .linkage = .dynamic,
         .name = "df_translation_library",
         .root_module = lib_mod,
     });
