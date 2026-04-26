258f6e48e996dcf7f4badc15c0eb2d680a32466b
insolor <2442833+insolor@users.noreply.github.com>
2026-04-26 21:50:35 +0300

    zig-cli usage fixes

--- cli/main.zig
+++ cli/main.zig
@@ -8,6 +8,7 @@ const zig_cli = @import("zig-cli");
 
 const app_spec = zig_cli.app(.{
     .name = "df_translation_library_cli",
+    .version = "0.0.0",
     .description = "CLI for the DF Translation Library",
     .commands = .{
         .print_mo = zig_cli.command(.{
@@ -26,17 +27,19 @@ const app_spec = zig_cli.app(.{
             },
         }),
     },
+    .global_options = .{},
 });
 
 pub fn main(init: std.process.Init) !void {
-    // const arena: std.mem.Allocator = init.arena.allocator();
+    const arena: std.mem.Allocator = init.arena.allocator();
 
     const io = init.io;
 
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
-    const args = zig_cli.parse(app_spec, init.minimal.args, std.heap.page_allocator) catch |err| switch (err) {
+    var args_iter = init.minimal.args.iterate();
+    const args = zig_cli.parseApp(app_spec, &args_iter, arena) catch |err| switch (err) {
         error.HelpRequested, error.VersionRequested => std.process.exit(0),
         else => std.process.exit(1),
     };
@@ -44,7 +47,7 @@ pub fn main(init: std.process.Init) !void {
     switch (args) {
         .print_mo => |opts| {
             std.debug.print("print_mo\n", .{});
-            std.debug.print("file_path={d}\n", .{
+            std.debug.print("file_path={s}\n", .{
                 opts.file_path,
             });
         },
