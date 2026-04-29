80d27a2bda1641ee41ef6320414e53137d6fc901
insolor <2442833+insolor@users.noreply.github.com>
2026-04-29 13:03:37 +0300

    Reorganize code in cli

--- cli/build.zig
+++ cli/build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "cli",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("main.zig"),
+            .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
