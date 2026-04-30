f3418d6d4d1e9651a0fc762df68ebd3516c4de89
insolor <2442833+insolor@users.noreply.github.com>
2026-04-30 10:57:09 +0300

    Move StringInterner to its own module

--- src/dictionary.zig
+++ src/dictionary.zig
@@ -1,56 +1,7 @@
 const std = @import("std");
 
 const parse_mo = @import("parse_mo.zig");
-
-/// Interner for strings stored in dictionary keys.
-const StringInterner = struct {
-    map: std.StringHashMap(void),
-    allocator: std.mem.Allocator,
-
-    const Self = @This();
-
-    /// Initialize interner.
-    pub fn init(allocator: std.mem.Allocator) Self {
-        return .{
-            .map = std.StringHashMap(void).init(allocator),
-            .allocator = allocator,
-        };
-    }
-
-    /// Deininialize interner.
-    pub fn deinit(self: *Self) void {
-        var it = self.map.keyIterator();
-        while (it.next()) |key| {
-            self.allocator.free(key.*);
-        }
-        self.map.deinit();
-    }
-
-    /// Create a copy of a string if it has not been stored in the internal map yet,
-    /// otherwise return exisitng string copy from the map.
-    pub fn intern(self: *Self, string: []const u8) ![]const u8 {
-        if (self.map.getKey(string)) |existing| {
-            return existing;
-        }
-
-        const copy = try self.allocator.dupe(u8, string);
-        try self.map.put(copy, {});
-        return copy;
-    }
-};
-
-test "Interner" {
-    const allocator = std.testing.allocator;
-    var interner = StringInterner.init(allocator);
-    defer interner.deinit();
-
-    const test_string: []const u8 = "test1";
-    const interned_string1 = try interner.intern(test_string);
-    try std.testing.expectEqualStrings(test_string, interned_string1);
-    try std.testing.expect(test_string.ptr != interned_string1.ptr);
-    const interned_string2 = try interner.intern(test_string);
-    try std.testing.expect(interned_string1.ptr == interned_string2.ptr);
-}
+const StringInterner = @import("StringInterner.zig");
 
 pub const DictionaryKey = struct {
     original_string: []const u8,
